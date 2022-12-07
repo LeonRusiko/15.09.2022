f62030d7978ac341c6302c011ed1574420cfc3ce
Aituar Semeibek <113921403+LeonRusiko@users.noreply.github.com>
2022-12-07 16:44:38 +0600

    Create task 4.js

---
+++ task 4.js
@@ -0,0 +1,11 @@
+let a = [1,312,523,2342,42,4,32,10,44,21]
+let c = []
+function b(){
+    for(let i = 0;i<a.length;i++){
+        if(a[i]%2 == 0){
+            c.push(a[i])
+        }
+    }
+    console.log(c);
+}
+b()
